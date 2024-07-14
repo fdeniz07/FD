d1c23aa02715d1df976a56a9e37760eb5d8d60fb
Fatih Deniz <deniz.fatih@hotmail.de>
2024-07-14 18:05:58 +0200

    AI Part + Taliwindcss Integration

---
+++ FD.Blazor.OpenAIStarter/Program.cs
@@ -0,0 +1,20 @@
+using FD.Blazor.OpenAIStarter;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.Extensions;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var openAIApiKey = builder
+    .Configuration
+    .GetSection("OpenAIApiKey")
+    .Value;
+
+builder.Services.AddOpenAIService(settings => settings.ApiKey = openAIApiKey);
+
+await builder.Build().RunAsync();
