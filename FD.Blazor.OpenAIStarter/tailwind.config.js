6b639eb7f2834b3e0c39d3ae7b25d88d63fdfc88
Fatih Deniz <deniz.fatih@hotmail.de>
2024-07-17 21:08:12 +0200

    Added ChatGpt and Dall-e integration

--- FD.Blazor.OpenAIStarter/tailwind.config.js
+++ FD.Blazor.OpenAIStarter/tailwind.config.js
@@ -2,12 +2,32 @@
 module.exports = {
     content: ['./**/*.{razor,html,cshtml}'],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                buttonheartbeat: 'buttonheartbeat 2s infinite ease-in-out',
+            },
+            keyframes: {
+                buttonheartbeat: {
+                    '0%': {
+                        'box-shadow': '0 0 0 0 theme("colors.yellow.500")',
+                        transform: 'scale(1)',
+                    },
+                    '50%': {
+                        'box-shadow': '0 0 0 7px theme("colors.yellow.500/0")',
+                        transform: 'scale(1.05)',
+                    },
+                    '100%': {
+                        'box-shadow': '0 0 0 0 theme("colors.yellow.500/0")',
+                        transform: 'scale(1)',
+                    },
+                },
+            },
+        },
     },
     plugins: [
         require('daisyui'),
     ],
     daisyui: {
-        themes: ["light", "dark", "cupcake", "synthwave"],
+        themes: ["light", "dark", "synthwave"],
     },
 }
