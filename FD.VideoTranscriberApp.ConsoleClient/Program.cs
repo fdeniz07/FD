62396352bf8e7c4829f18ca7c7732dbbc5c1b960
Fatih Deniz <deniz.fatih@hotmail.de>
2024-07-24 20:17:50 +0200

    VideoTranscriberApp

---
+++ FD.VideoTranscriberApp.ConsoleClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using Xabe.FFmpeg;
+
+var videoFilePath = "C:\\Videos\\Cat.mp4";
+
+//Get audio file path
+//string audioFilePath = Path.ChangeExtension(Path.GetTempFileName(), "mp3");
+string audioFilePath = Path.ChangeExtension(videoFilePath, "mp3");
+
+CancellationTokenSource cts = new CancellationTokenSource();
+
+//CancelationToken --> Asenkron islemlerinde mutlaka kullanilmasi gerekir.
+
+var conversion = await FFmpeg.Conversions.FromSnippet.ExtractAudio(videoFilePath, audioFilePath);
+
+await conversion.Start(cts.Token);
+
+Console.WriteLine("Conversion completed.");
+
+Console.WriteLine(audioFilePath);
