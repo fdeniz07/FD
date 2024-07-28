70a0f39a386443eb18a2013e762f8c24e0a7f885
Fatih Deniz <deniz.fatih@hotmail.de>
2024-07-28 23:09:59 +0200

    Client Side updates

---
+++ FD.VideoTranscriberApp.BlazorUI/FD.VideoTranscriberApp.BlazorUI.Client/Enums/ButtonSize.cs
@@ -0,0 +1,10 @@
+﻿namespace FD.VideoTranscriberApp.BlazorUI.Client.Enums
+{
+    public enum ButtonSize
+    {
+        ExtraSmall = 1,
+        Small = 2,
+        Medium = 3,
+        Large = 4
+    }
+}
